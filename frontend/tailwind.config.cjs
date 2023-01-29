2b44b4e42525c533bf1dcdb216586de453b3746f
Anes Khadiri <anes1999@live.ca>
2023-01-29 17:35:44 -0500

    Integrated tailwind CSS in the front-end

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "at-light-green": "#44ba9f",
+        "at-green": "#286d5d",
+        "light-grey": "#f1f1f1",
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
+      },
+      screens : {
+        'xs':'319px',
+      }
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    flex: {
+      "1": "1 1 0%",
+      "2": "2 2 0%",
+    },
+    container: {
+      center: true,
+      screens: {
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1300px",
+      },
+    },
+  },
+  variants: {
+ 
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
+};
