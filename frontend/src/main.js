f1f870924f402cf4ab1404d8dfbcdd99d7af4631
Anes Khadiri <anes1999@live.ca>
2023-01-29 17:42:14 -0500

    Integrated pinia store in the front-end

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router/index";
 import '../src/style.css'
 
+const pinia = createPinia();
 const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
 app.mount("#app")
