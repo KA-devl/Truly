c8931f447147e121cdd841964439c7ef3fdd8865
Anes Khadiri <anes1999@live.ca>
2023-02-12 20:09:07 -0500

    Removed darkMode from tailwind file

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
-  darkMode: false, // or 'media' or 'class'
+  // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
