eb4ff76e61213367e820f9dae5ff5a8a4faabdbe
MikelsonB <mickelson2010@hotmail.com>
2023-02-14 12:44:38 -0500

    refactoring the jobPosting root

--- backend/routes/job-posting/jobPosting.js
+++ backend/routes/job-posting/jobPosting.js
@@ -1,18 +1,16 @@
-
-const jobPosting = require("../../models/jobpost");
+const jobPosting = require('../../models/jobpost');
 
 //CREATE A JOBPOSTING ( its better to define a name to the function so that we can understand what it does)
 
-const createJobPosting = (app) =>{
+const createJobPosting = (app) => {
+  app.post('/api/create-job', async (req, res) => {
+    try {
+      const jobpost = await jobPosting.create(req.body);
+      res.status(201).json({ sucess: true, data: jobpost });
+    } catch (err) {
+      res.status(400).json({ sucess: false, message: err.message });
+    }
+  });
+};
 
-app.post("/api/create-job-post", async (req, res) => {
-    try{
-    const jobpost=  await jobPosting.create(req.body);
-    res.status(200).json({sucess:true,data:jobpost});
-    }
-    catch(err){
-    res.status(500).json({sucess:false, message:err.message});}
-    })
-    }
-    
-module.exports =createJobPosting;
+module.exports = createJobPosting;
