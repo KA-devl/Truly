ab982dc8c2ac6fc1d3a591e7492e6d5bceb38017
MikelsonB <mickelson2010@hotmail.com>
2023-02-14 08:30:17 -0500

    refactoring the createUser root

--- backend/routes/user/createUser.js
+++ backend/routes/user/createUser.js
@@ -1,30 +1,21 @@
-const User = require("../../models/user");
-const mongoose = require("mongoose");
+const user = require("../../models/user");
 
-module.exports = (app) =>
+// its better to define a name for this function so that we can understand what it does)
+const createUser= (app) =>
   app.post("/api/signup", async (req, res) => {
-    //sample user
-    // let user = new User({
-    //   name: "anesssds",
-    //   email: "test123@live.com33",
-    //   username: "bawfgsdffg",
-    //   age: "60",
-    //   mobileNumber: "514-999-999594",
-    //   userType: "candidate",
-    // });
-    let user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      username: req.body.username,
-      age: req.body.age,
-      mobileNumber: req.body.mobileNumber,
-      userType: req.body.userType,
-    });
+    
     try {
-      const newUser = await user.save();
+      // // since newUser is a model we 
+      // can directly use the CRUD METHOD 
+      // on  it. Here we  Passing directly  all the body fields 
+      // to the CRUD operation create. Then the new "instance" created is assigned to  newUser variable 
+      const newUser = await user.create(req.body);
       let message = 'The account was successfully created'
-      res.status(201).json({message, data: newUser});
+      res.status(201).json({ sucess:true,message, data: newUser});
     } catch (error) {
-      res.status(400).json({ message: error.message });
+      res.status(400).json({ sucess:false,message: error.message });
     }
   });
+
+  // The above method is exported so that it can be availaible in app.js
+  module.exports =createUser;
