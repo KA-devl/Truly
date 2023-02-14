6c11da80a50e29e2019b3687a65b4de77cb4e9db
MikelsonB <mickelson2010@hotmail.com>
2023-02-14 07:26:31 -0500

    Implementation of a try/ catch block to handle error in jobposting root

--- backend/routes/job-posting/jobPosting.js
+++ backend/routes/job-posting/jobPosting.js
@@ -2,10 +2,17 @@
 const jobPosting = require("../../models/jobpost");
 
 //CREATE A JOBPOSTING ( its better to define a name to the function so that we can understand what it does)
-const createJobPosting = (app) =>
-app.post("/api/create-job-post", async (req, res) => {
-    const jobpost=  await jobPosting.create(req.body);
-    res.status(200).json({sucess:true,data:jobpost})
-})
 
+const createJobPosting = (app) =>{
+
+app.post("/api/create-job-post", async (req, res) => {
+    try{
+    const jobpost=  await jobPosting.create(req.body);
+    res.status(200).json({sucess:true,data:jobpost});
+    }
+    catch(err){
+    res.status(500).json({sucess:false});}
+    })
+    }
+    
 module.exports =createJobPosting;
