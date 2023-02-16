232bf929ee45b8608a57de7d9e19be7dca86e68e
Anes Khadiri <anes1999@live.ca>
2023-02-16 16:41:40 -0500

    Changed user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,58 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 /**
  * User Schema
  */
 const userSchema = new mongoose.Schema({
-  name:{
+  name: {
     type: String,
     required: true,
-    trim:true,
-    maxlength :50
+    trim: true,
+    maxlength: 50,
   },
-  email:{
+  email: {
     type: String,
-    match: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    match:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     required: true,
-    unique: true
+    unique: true,
   },
   username: {
     type: String,
     required: true,
-    unique : true,
-    trim:true,
-    maxlength :50
+    unique: true,
+    trim: true,
+    maxlength: 50,
   },
-  age: {
-    type : Number,
-    required : true,
-    min: 18,
-    max: 110,
-
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 50,
   },
   mobileNumber: {
     type: String,
-    match : /^(\(\+[0-9]{2}\))?([0-9]{3}-?)?([0-9]{3})\-?([0-9]{4})(\/[0-9]{4})?$/ ,
-    unique : true,
-    required: true
-    
+    match:
+      /^(\(\+[0-9]{2}\))?([0-9]{3}-?)?([0-9]{3})\-?([0-9]{4})(\/[0-9]{4})?$/,
+    unique: true,
+    required: true,
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   userType: {
     type: String,
-    required : true
-  }
+    required: true,
+  },
 });
 
-userSchema.post('save', function(error, doc, next) {
-  if (error.name === 'MongoServerError' && error.code === 11000) {
-    next(new Error('The user already exists'));
+userSchema.post("save", function (error, doc, next) {
+  if (error.name === "MongoServerError" && error.code === 11000) {
+    next(new Error("The user already exists"));
   } else {
     next();
   }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
