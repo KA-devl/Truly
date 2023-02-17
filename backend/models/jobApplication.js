b2a08062dc89cb5e76fef5320bc6b26b8873338c
MikelsonB <mickelson2010@hotmail.com>
2023-02-16 23:22:50 -0500

    Implementation of the Job application module

---
+++ backend/models/jobApplication.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const User = require('../models/user');
+
+const JobApplicationSchema = new mongoose.Schema({
+  candidateId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'user',
+    required: true,
+    validate: {
+      validator: async function (value) {
+        const user = await User.findOne({ _id: value, userType: 'candidate' });
+        return !!user;
+      },
+      message:
+        'candidateId must be the ObjectId of a user with userType "candidate"',
+    },
+  },
+
+  jobPostId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'jobPost',
+    required: true,
+  },
+
+  applicationStatus: {
+    type: [String],
+    required: true,
+    enum: ['pending', 'under review', 'rejected'],
+  },
+
+  creationDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('jobApplication', JobApplicationSchema);
