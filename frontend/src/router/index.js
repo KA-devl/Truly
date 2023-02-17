04fa8909c82e87889d0dc50f600a2a3f2539e0b6
SlygoPika <hmorindg25@gmail.com>
2023-02-17 18:17:17 -0500

    added basic employer dashboard table [WIP]

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import displayList from '../views/DisplayList.vue';
 import Login from '../components/Login.vue'
+import EmployerDashboard from '../views/EmployerDashboard.vue';
 import SignUp from '../components/SignUp.vue'
 
 const routes = [
@@ -18,6 +19,11 @@ const routes = [
   name: "SignUp",
   component : SignUp,
   path: "/signup",
+},
+{
+  name: "EmployerDashboard",
+  component : EmployerDashboard,
+  path: "/employerdashboard",
 }
 
 ]
