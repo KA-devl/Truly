92629b82436728432abb5b35b984ec1eaa1ae373
Anes Khadiri <anes1999@live.ca>
2023-02-16 20:45:18 -0500

    Added sidebar for logged in users

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <Navbar/>
+  <Navbar v-if="!user"/>
+  <UserSideBar v-if="user"/>
   <router-view />
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
+import Navbar from './components/Navbar.vue';
+import UserSideBar from './components/UserSideBar.vue';
+import { useUserStore } from './store/user';
 export default {
   name: 'App',
   components:{
-    Navbar
+    Navbar,
+    UserSideBar
+  },
+  setup(){
+    const user = useUserStore().user;
+
+    return {user};
   }
 }
 </script>
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,25 +1,56 @@
-<template>
-  <!-- Both candidate and emplyer have same dashboard. To display the data for candidate, you would have something like: <div v-if userType=="candidate"> .... show the user dashbaord </div>-->
-  <h1>USER {{user}}</h1>
+<template><!-- Both candidate and emplyer have same dashboard. To display the data for candidate, you would have something like: <div v-if userType=="candidate"> .... show the user dashbaord </div>-->
 
-  
+  <div class="p-4 sm:ml-64">
+    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+      <!-- ALL YOUR CODE MUST BE INSIDE THIS TAG (OR ELSE IT WILL CAUSE OVERFLOW) -->
+      <h2 class="text-3xl font-semibold ">Welcome back, {{ user.data.name }}</h2>
+      <div class="grid grid-cols-3 gap-4 mb-4 mt-4">
+        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+        </div>
+        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+        </div>
+        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+        </div>
+      </div>
+      <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+      </div>
+      <div class="grid grid-cols-2 gap-4 mb-4">
+        <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+        </div>
+        <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+        </div>
+        <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+        </div>
+        <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+        </div>
+ 
+ 
 
-
-
-
+      
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 //import { onMounted } from 'vue';
 import { useUserStore } from '../store/user';
 export default {
-  setup(){
+  setup() {
     const user = useUserStore().user;
 
 
- 
 
 
-    return {user}
+
+    return { user }
   }
 }
 
@@ -28,9 +59,4 @@ export default {
 
 </script>
 
-<style>
-
-
-
-
-</style>
+<style></style>
