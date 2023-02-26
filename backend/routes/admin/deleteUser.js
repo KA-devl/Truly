da9afb18114463383072351f4f5bb62a7929afea
mickelson benjamin <mickelson2010@hotmail.com>
2023-02-26 15:08:49 -0500

    Deleting avatar and resume from the cloud when a user is deleted

--- backend/routes/admin/deleteUser.js
+++ backend/routes/admin/deleteUser.js
@@ -1,11 +1,15 @@
 const userModel = require('../../models/user');
+const cloudinary = require('../../config/cloudinary');
 
 const deleteUser = (app) =>
   app.delete('/api/delete-user/:id', async (req, res) => {
     try {
       const user = await userModel.findById(req.params.id);
+      await cloudinary.uploader.destroy(user.resume.cloudinaryId);
+      await cloudinary.uploader.destroy(user.avatar.cloudinaryId);
       user.remove();
-      if (user === null)
+      console.log('user deleted successfully');
+      if (!user)
         return res
           .status(400)
           .json({ sucess: false, message: `This user wasnt found` });
