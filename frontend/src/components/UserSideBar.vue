73501f473918355e85766dd3ddc3a72187e2e0d0
Sanassah <anas.senouci@gmail.com>
2023-03-08 13:16:08 -1000

    Dashboard modification to add a widget of notifications issue #69

--- frontend/src/components/UserSideBar.vue
+++ frontend/src/components/UserSideBar.vue
@@ -9,14 +9,14 @@
       </a>
       <ul class="space-y-2">
          <li>
-            <router-link :to="{name: 'Dashboard'}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
+            <router-link :to="{name: 'Dashboard'}" class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
                <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
                <span class="ml-3">Dashboard</span>
             </router-link>
          </li>
          <li v-if="user.userType === 'employer'">
             <!-- v-if userType is employer -->
-            <router-link :to="{name:'CreateJob'}" href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
+            <router-link :to="{name:'CreateJob'}" href="#" class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
                <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75  group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
                <span class="flex-1 ml-3 whitespace-nowrap">Create job</span>
              
@@ -24,20 +24,26 @@
          </li>
          <li v-if="user.userType === 'candidate'">
             <!-- v-if userType is employer -->
-            <router-link :to="{name:'MyResumev2'}" href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
+            <router-link :to="{name:'MyResumev2'}" href="#" class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
                <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75  group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
                <span class="flex-1 ml-3 whitespace-nowrap">My Resume</span>
              
             </router-link>
          </li>
          <li>
-            <router-link :to="{name:'EditUserProfile'}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
+            <router-link :to="{name:'EditUserProfile'}" class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
                <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75  group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
                <span class="flex-1 ml-3 whitespace-nowrap">Profile settings</span>
             </router-link>
          </li>
+         <li v-if="user.userType === 'candidate'">
+            <router-link :to="{name:'EditUserProfile'}" class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
+               <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path></svg>
+               <span class="flex-1 ml-3 whitespace-nowrap">Saved Jobs</span>
+            </router-link>
+         </li>
          <li class="cursor-pointer">
-            <div @click="logout" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
+            <div @click="logout" class="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
                <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75  group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>
                <span class="flex-1 ml-3 whitespace-nowrap">Sign out</span>
             </div>
--- frontend/src/views/CandidateDashboard.vue
+++ frontend/src/views/CandidateDashboard.vue
@@ -1,47 +1,65 @@
 <template><!-- Both candidate and emplyer have same dashboard. To display the data for candidate, you would have something like: <div v-if userType=="candidate"> .... show the user dashbaord </div>-->
     
         <!-- ALL YOUR CODE MUST BE INSIDE THIS TAG (OR ELSE IT WILL CAUSE OVERFLOW) -->
+
+                  <!-- Dashboard stats start -->
+                  <div class="max-w-screen-lg px-4 md:px-8 mx-auto">
+              <div class=" grid grid-cols-2 md:grid-cols-4 bg-blue-500 rounded-lg gap-6 md:gap-8 p-6 md:p-8 " style="position: absolute; right:45px; top=:10px;">
+                <!-- stat - start -->
+                <div class="flex flex-col items-center">
+                  <div class="text-white text-xl sm:text-2xl md:text-3xl font-bold">4</div>
+                  <div class="text-indigo-200 text-sm sm:text-base">New Jobs</div>
+                </div>
+                <!-- stat - end -->
+
+                <!-- stat - start -->
+                <div class="flex flex-col items-center">
+                  <div class="text-white text-xl sm:text-2xl md:text-3xl font-bold">7</div>
+                  <div class="text-indigo-200 text-sm sm:text-base">Jobs Applied</div>
+                </div>
+                <!-- stat - end -->
+
+                <!-- stat - start -->
+                <div class="flex flex-col items-center">
+                  <div class="text-white text-xl sm:text-2xl md:text-3xl font-bold">3</div>
+                  <div class="text-indigo-200 text-sm sm:text-base">Jobs Saved</div>
+                </div>
+
+                <!-- stat - start -->
+                <div class="flex flex-col items-center">
+                  <div class="text-white text-xl sm:text-2xl md:text-3xl font-bold">14</div>
+                  <div class="text-indigo-200 text-sm sm:text-base">people have viewed your profile</div>
+                </div>
+                <!-- stat - end -->
+              </div>
+            </div>
+            <!-- Dashboard stats end -->
+
         <h2 class="text-3xl font-semibold ">Dashboard</h2>
         <h2 class="text-2xl font-md text-gray-500 mt-4 ">Welcome back, <span class="text-blue-500">{{ user.name }}</span></h2>
-        <div class="flex flex-row justify-between w-full mb-1 mt-4 sm:mb-0">
+        <br> <br>
+        <div class="flex flex-row justify-between w-full mb-4 mt-6 sm:mb-0">
           <h2 class="text-2xl leading-tight">
             Open positions
           </h2>
           <div class="text-end">
             <form class="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
             <div class=" relative ">
-            <input type="text" id="&quot;form-subscribe-Filter" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:dark:bg-gray-800 focus:border-transparent" placeholder="Search"/>
+            <input type="text" id="&quot;form-subscribe-Filter" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:border-transparent" placeholder="Search"/>
             </div>
-              <button class="flex-shrink-0 px-4 py-2 text-base font-semibold text-white dark:bg-gray-800 hover:text-blue-500 focus:outline-none focus:ring-2 focus:dark:bg-gray-800 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
+              <button class="flex-shrink-0 px-4 py-2 text-base font-semibold text-white dark:bg-blue-500 hover:text-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
                 Filter
               </button>
             </form>
           </div>
         </div>
-        <div class="grid grid-cols-3 gap-4 mb-4 mt-4">
+        <div class="grid grid-cols-3 gap-4 mb-1 mt-1">
         </div>
-        <div class="flex items-center justify-center mb-4 rounded">
+        <div class="flex items-center justify-center mb-1 rounded">
           <div v-if="data" class=" w-full">
-           
             <PostingTable :data="data" :headers="headers" :user="user"/>
-            
         </div>
         </div>
-        <div class="grid grid-cols-2 gap-4 mb-4">
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
-          </div>
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
-          </div>
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
-          </div>
-          <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
-          </div>
-
-    </div>
   </template>
   
   <script>
