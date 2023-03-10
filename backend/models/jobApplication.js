9da002215ee951cb3deb010636c6c69e0931115f
MikelsonB <mickelson2010@hotmail.com>
2023-03-09 20:46:24 -0500

    refactoring jobApplication.js

--- backend/models/jobApplication.js
+++ backend/models/jobApplication.js
@@ -50,7 +50,6 @@ const JobApplicationSchema = new mongoose.Schema({
     default: ['active'],
     enum: ['under review', 'rejected', 'active', 'inactive'],
   },
-  cv: { type: String, required: true },
 
   creationDate: {
     type: Date,
