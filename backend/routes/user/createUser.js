13b9bb08da050a399e796df2c2223092b7b87d8a
mickelson benjamin <mickelson2010@hotmail.com>
2023-03-22 22:02:42 -0400

    refactoring the /api/signup endpoint  to transfert  its method implementation  into the controller createUser

--- backend/routes/user/createUser.js
+++ backend/routes/user/createUser.js
@@ -1,10 +1,4 @@
-
-const { signUpUserController} = require('../../controllers/user/createUser');
+const { signUpUserController } = require('../../controllers/user/createUser');
 
 // CREATE A USER
-// its better to define a name for this function so that we can understand what it does)
-const createUser = (app) =>
-  app.post('/api/signup', signUpUserController );
-
-// The above method is exported so that it can be availaible in app.js
-module.exports = createUser;
+module.exports = (app) => app.post('/api/signup', signUpUserController);
