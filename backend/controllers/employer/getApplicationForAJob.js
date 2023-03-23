04e8d764d897b5cf8f78dc82fce61b27ca50c11e
mickelson benjamin <mickelson2010@hotmail.com>
2023-03-23 18:58:45 -0400

    refactoring api/get-application-job/:jobid  endpoint  to transfert  its method implementation  into the controller getApplicationForAJob

---
+++ backend/controllers/employer/getApplicationForAJob.js
@@ -0,0 +1,24 @@
+const Application = require('../../models/jobApplication');
+
+// GET ALL THE APPLICATIONS FOR A SPECIFIC JOB POST BY ITS ID
+async function getApplicationJob(req, res) {
+  try {
+    const getAllApplication = await Application.find({
+      jobPostId: req.params.jobid,
+    }).populate({
+      path: 'candidateId',
+      model: 'user',
+    });
+    if (!getAllApplication)
+      return res.status(400).json({
+        sucess: false,
+        message: `No job application was found for this job post`,
+      });
+
+    res.status(201).json({ sucess: true, data: getAllApplication });
+  } catch (err) {
+    res.status(400).json({ sucess: false, message: err.message });
+  }
+}
+
+module.exports = { getApplicationJob };
