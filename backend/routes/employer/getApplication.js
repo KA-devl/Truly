b995becddc86fb967df3be510b32457e9521b779
mickelson benjamin <mickelson2010@hotmail.com>
2023-03-23 18:49:24 -0400

    refactoring /api/get-application-employer/:id  endpoint  to transfert  its method implementation  into the controller getApplication

--- backend/routes/employer/getApplication.js
+++ backend/routes/employer/getApplication.js
@@ -1,25 +1,6 @@
-const Application = require('../../models/jobApplication');
+const { getApplication } = require('../../controllers/employer/getApplication');
 
 // GET ALL THE JOB APPLICATIONS THAT A EMPLOYER RECEIVED
-const getApplication = (app) => {
-  app.get('/api/get-application-employer/:id', async (req, res) => {
-    try {
-      const getAllApplication = await Application.find({
-        authorId: req.params.id,
-      });
-      if (!getAllApplication)
-        return res.status(400).json({
-          sucess: false,
-          message: `No job application was found for this employer`,
-        });
-
-      console.log(typeof getAllApplication);
-
-      res.status(201).json({ sucess: true, data: getAllApplication });
-    } catch (err) {
-      res.status(400).json({ sucess: false, message: err.message });
-    }
-  });
+module.exports = (app) => {
+  app.get('/api/get-application-employer/:id', getApplication);
 };
-
-module.exports = getApplication;
