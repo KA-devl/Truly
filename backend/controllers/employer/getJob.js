a144185db41f83e95d9b291aa72d31b907b9f6b3
mickelson benjamin <mickelson2010@hotmail.com>
2023-03-23 19:05:07 -0400

    refactoring /api/get-job/:id  endpoint  to transfert  its method implementation  into the controller getJob

---
+++ backend/controllers/employer/getJob.js
@@ -0,0 +1,19 @@
+const jobPosting = require('../../models/jobPost');
+
+// GET A JOB BY ITS ID
+async function getJob(req, res) {
+  try {
+    const jobpost = await jobPosting.findById(req.params.id);
+
+    if (!jobpost)
+      return res.status(400).json({ sucess: false, message: `invalid id` });
+
+    res.status(201).json({ sucess: true, data: jobpost });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ sucess: false, message: `The job id entered is invalid` });
+  }
+}
+
+module.exports = { getJob };
--- backend/routes/employer/getJob.js
+++ backend/routes/employer/getJob.js
@@ -1,21 +1,6 @@
-const jobPosting = require('../../models/jobPost');
+const { getJob } = require('../../controllers/employer/getJob');
 
 // GET A JOB BY ITS ID
-const getJob = (app) => {
-  app.get('/api/get-job/:id', async (req, res) => {
-    try {
-      const jobpost = await jobPosting.findById(req.params.id);
-
-    if (!jobpost)
-        return res.status(400).json({ sucess: false, message: `invalid id` });
-
-      res.status(201).json({ sucess: true, data: jobpost });
-    } catch (err) {
-      res
-        .status(400)
-        .json({ sucess: false, message: `The job id entered is invalid` });
-    }
-  });
+module.exports = (app) => {
+  app.get('/api/get-job/:id', getJob);
 };
-
-module.exports = getJob;
