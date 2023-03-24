40707642ea824065cf145528c862c4e590b99434
mickelson benjamin <mickelson2010@hotmail.com>
2023-03-23 22:02:27 -0400

    refactoring /api/delete-user/:id  endpoint  to transfert  its method implementation  into the controller deleteUser

---
+++ backend/controllers/admin/deleteUser.js
@@ -0,0 +1,27 @@
+const userModel = require('../../models/user');
+const cloudinary = require('../../config/cloudinary');
+
+async function deleteUser(req, res) {
+  try {
+    const user = await userModel.findById(req.params.id);
+    // delete the user resume from the cloud
+    await cloudinary.uploader.destroy(user.resume.cloudinaryId);
+    // delete the user avatar from the cloud
+    await cloudinary.uploader.destroy(user.avatar.cloudinaryId);
+    user.remove();
+    console.log('user deleted successfully');
+    if (!user)
+      return res
+        .status(400)
+        .json({ sucess: false, message: `This user wasnt found` });
+
+    res.status(201).json({
+      sucess: true,
+      message: `User with id ${req.params.id} has been deleted`,
+      data: user,
+    });
+  } catch (err) {
+    res.status(400).json({ sucess: false, message: `The user id is invalid` });
+  }
+}
+module.exports = { deleteUser };
