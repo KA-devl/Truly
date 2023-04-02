09e54b142edaafd096487b3bae1c745e2e57702e
mickelson benjamin <mickelson2010@hotmail.com>
2023-04-01 23:38:34 -0400

    unit test fort the getAllinterview refactored

--- backend/__tests__/controllers/getAllinterviews.test.js
+++ backend/__tests__/controllers/getAllinterviews.test.js
@@ -1,7 +1,7 @@
-const supertest = require('supertest');
-const { app, server } = require('../../app');
 const Application = require('../../models/jobApplication');
-const mongoose = require('mongoose');
+const {
+  getAllinterviews,
+} = require('../../controllers/candidate/getAllinterviews');
 
 // Mock the Job Application model
 jest.mock('../../models/jobApplication', () => ({
@@ -12,61 +12,46 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-afterAll(async () => {
-  await server.close();
-});
+const response = {
+  status: jest.fn((x) => ({
+    json: jest.fn((x) => x),
+  })),
+  json: jest.fn((x) => x),
+  send: jest.fn((x) => x),
+};
 
-afterAll(async () => {
-  await mongoose.connection.close();
-});
+const request = {
+  params: jest.fn((x) => x),
+};
+
 describe('getAllinterviews', () => {
   it('should return all interviews for a candidate', async () => {
     // Mock the find function to return a list of job applications
     const jobApplications = [
-      { jobTitle: 'Software Engineer' },
-      { jobTitle: 'Web Developer' },
+      {
+        _id: '6420a1964e8ee54cf530f767',
+        candidateId: '641d2207b47d92a84e53d649',
+        jobPostId: '641fbd0f1c79826f993f9e4d',
+        applicationStatus: ['interview'],
+        authorId: '641fbcc71c79826f993f9e35',
+      },
+      {
+        _id: '642633f638125bfae5ba8435',
+        candidateId: '641d2207b47d92a84e53d649',
+        jobPostId: '6426319d38125bfae5ba82b8',
+        applicationStatus: ['interview'],
+        authorId: '6426318938125bfae5ba82a1',
+      },
     ];
-    Application.find.mockResolvedValue(jobApplications);
+    Application.find.mockResolvedValueOnce(jobApplications);
 
-    // Make a GET request to the controller
-    const response = await supertest(app).get('/api/get-all-interviews/123');
-
-    // Assertions
-    expect(Application.find).toHaveBeenCalledWith({
-      candidateId: '123',
-      applicationStatus: ['interview'],
-    });
-    expect(response.statusCode).toBe(201);
+    await getAllinterviews(request, response);
+    expect(response.status).toHaveBeenCalledWith(201);
   });
 
-  it('should return an error message if no interviews are found', async () => {
-    // Mock the find function to return an empty array
-    Application.find.mockResolvedValue([]);
-
-    // Make a GET request to the controller
-    const response = await supertest(app).get('/api/get-all-interviews/123');
-
-    // Assertions
-    expect(Application.find).toHaveBeenCalledWith({
-      candidateId: '123',
-      applicationStatus: ['interview'],
-    });
-    expect(response.statusCode).toBe(200);
-  });
-
-  it('should return an error message if an error occurs', async () => {
-    // Mock the find function to throw an error
-    const errorMessage = 'Internal server error';
-    Application.find.mockRejectedValue(new Error(errorMessage));
-
-    // Make a GET request to the controller
-    const response = await supertest(app).get('/api/get-all-interviews/123');
-
-    // Assertions
-    expect(Application.find).toHaveBeenCalledWith({
-      candidateId: '123',
-      applicationStatus: ['interview'],
-    });
-    expect(response.statusCode).toBe(400);
+  it('should return a message if no interviews are found', async () => {
+    Application.find.mockResolvedValueOnce([]);
+    await getAllinterviews(request, response);
+    expect(response.status).toHaveBeenCalledWith(200);
   });
 });
