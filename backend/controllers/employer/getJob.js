4aa2278c4e0ab8a12a011bd46e28f6c49eea6bf0
Anes Khadiri <anes1999@live.ca>
2023-04-09 15:20:54 -0400

    Populated job to get more information about user

--- backend/controllers/employer/getJob.js
+++ backend/controllers/employer/getJob.js
@@ -3,7 +3,10 @@ const jobPosting = require('../../models/jobPost');
 // GET A JOB BY ITS ID
 async function getJob(req, res) {
   try {
-    const jobpost = await jobPosting.findById(req.params.id);
+    const jobpost = await jobPosting.findById(req.params.id).populate({
+      path: 'authorId',
+      model: 'user',
+    });
 
     if (!jobpost)
       return res.status(400).json({ sucess: false, message: `invalid id` });
