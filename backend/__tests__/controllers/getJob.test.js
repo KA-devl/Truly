809088f18c112eed76bfaca924035bd4781f3601
Sanassah <anas.senouci@gmail.com>
2023-04-12 23:35:25 -0800

    Correction Sonarqube errors

--- backend/__tests__/controllers/getJob.test.js
+++ backend/__tests__/controllers/getJob.test.js
@@ -17,16 +17,6 @@ const request = {
   params: jest.fn((x) => x),
 };
 
-// it('should return a 201  if job postings are found', async () => {
-//   jobPosting.findById.mockResolvedValueOnce([{ Jobtitle: 'test' }]);
-
-//   // Act
-//   await getJob(request, response);
-
-//   // Assert
-//   expect(response.status).toHaveBeenCalledWith(201);
-// });
-
 it('should return a 400 if no  job postings are found', async () => {
   jobPosting.findById.mockResolvedValueOnce([]);
 
